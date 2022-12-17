097c0822e6ac2bad76bc56785b0b157018c4d807
Hitesh <gaikwadhitesh03@gmail.com>
2022-12-17 13:48:37 +0530

    change

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,22 @@
+package com.driver;
+
+  public class Boat implements WaterVehicle{
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    private String name;
+    private int capacity;
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,6 +4,8 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super.name=name;
+        this.isManual=isManual;
     }
 
     public void accelerate(int rate){
@@ -17,14 +19,33 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
+        newSpeed+=rate;
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            this.changeGear(1);
+            stop();
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+            //changeSpeed(newSpeed,getCurrentDirection());
+            if(newSpeed>0 && newSpeed<=50){
+                this.changeGear(1);
+            }else if(newSpeed>50 && newSpeed<=100){
+                    this.changeGear(2);
+            }else if(newSpeed>100 && newSpeed<=150){
+                this.changeGear(3);
+            }else if(newSpeed>150 && newSpeed<=200){
+                this.changeGear(4);
+            }else if(newSpeed>200 && newSpeed<=250){
+                this.changeGear(5);
+            }else if(newSpeed>250){
+                this.changeGear(6);
+            }
         }
     }
+
+//    private void getCurrentDirection() {
+//    }
+
 }
